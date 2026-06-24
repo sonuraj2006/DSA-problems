24b6c40fb1e339df3fbef6bbc10d6a5b46c337a1
sonuraj2006 <nehasonubanoth05@gmail.com>
2026-06-24 19:03:23 +0530

    Two pointers method

---
+++ move_zero.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Solution sol = new Solution();
+        sol.moveZeroes(nums);
+        System.out.println("Array after moving zeroes:");
+        System.out.println(Arrays.toString(nums));
+        sc.close();
+    }
+}
